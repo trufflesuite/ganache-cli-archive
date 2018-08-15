33d11c611fe8f862d373d4bb4bf908a481ba5c71
Ben Burns <benjamin.c.burns@gmail.com>
2018-08-15 04:59:39 +0000

    ganache-core@2.2.0

--- webpack/base.webpack.config.js
+++ webpack/base.webpack.config.js
@@ -29,7 +29,7 @@ module.exports = function(override) {
         "electron": path.join(__dirname, "..", "./nil.js"),
         "ws": path.join(__dirname, "..", "./nil.js"),
         "scrypt": "js-scrypt",
-        "secp256k1": path.join(__dirname, "..", "node_modules", "secp256k1", "elliptic.js")
+        "secp256k1": path.join(__dirname, "..", "node_modules", "ganache-core", "node_modules", "secp256k1", "elliptic.js")
       }
     },
     mode: 'production'
