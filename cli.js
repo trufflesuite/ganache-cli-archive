cdb98ef5376a0c45815a30a15db567d7881daeb6
dennisjuanito <dennisjuanito@gmail.com>
2019-06-24 14:34:11 +0700

    Display Available Account addresses as EIP-55 checksum addresses when ganache-cli starts

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ require('source-map-support').install();
 
 var yargs = require("yargs");
 var pkg = require("./package.json");
+var createKeccakHash = require("keccak");
 var ganache;
 try {
   ganache = require("./lib");
@@ -41,6 +42,22 @@ function parseAccounts(accounts) {
     ret.push(splitAccount(accounts[i]));
   }
   return ret;
+}
+
+function toChecksumAddress (address) {
+  address = address.toLowerCase().replace('0x', '')
+  var hash = createKeccakHash('keccak256').update(address).digest('hex')
+  var ret = '0x'
+
+  for (var i = 0; i < address.length; i++) {
+    if (parseInt(hash[i], 16) >= 8) {
+      ret += address[i].toUpperCase()
+    } else {
+      ret += address[i]
+    }
+  }
+
+  return ret
 }
 
 if (argv.d) {
@@ -138,7 +155,7 @@ server.listen(options.port, options.hostname, function(err, result) {
 
   addresses.forEach(function(address, index) {
     var balance = new BN(accounts[address].account.balance).divRound(new BN("1000000000000000000")).toString();
-    var line = "(" + index + ") " + address + " (~" + balance + " ETH)";
+    var line = "(" + index + ") " + toChecksumAddress(address) + " (~" + balance + " ETH)";
 
     if (state.isUnlocked(address) == false) {
       line += " 🔒";
