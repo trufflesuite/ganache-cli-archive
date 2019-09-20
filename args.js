96041bb8b3d34fa15709ecf8a9316a8270371add
David Murdoch <david@davidmurdoch.com>
2019-09-20 19:57:15 -0400

    Istanbul support; Update ganache-core to v2.9.0-istanbul.0

--- args.js
+++ args.js
@@ -70,7 +70,7 @@ module.exports = exports = function(yargs, version, isDocker) {
       group: 'Chain:',
       alias: 'hardfork',
       type: 'string',
-      describe: "Allows users to specify which hardfork should be used. Supported hardforks are `byzantium`, `constantinople`, and `petersburg` (default).",
+      describe: "Allows users to specify which hardfork should be used. Supported hardforks are `byzantium`, `constantinople`, `petersburg` (default), and `istanbul` (beta).",
       default: "petersburg"
     })
     .option('f', {
