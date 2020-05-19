17b69c4a79ddb46ebb0138b2ae282178f6a80c81
David Murdoch <davidmurdoch@users.noreply.github.com>
2020-05-19 12:19:24 -0400

    build: set otp during release process (#753)
    * build: testing github actions
    
    * build: add `internal` branch to github actions
    
    * build: might as well try this
    
    * build: maybe it's a timing issue
    
    * build: add comment about intent
    
    * .
    
    * .
    
    * deparate
    
    * .
    
    * Try caching npm node modules
    
    * did it cache?
    
    * cache did nothing
    
    * Remove the "internal" release

--- scripts/generate-otp.js
+++ scripts/generate-otp.js
@@ -1,3 +1,6 @@
+const EOL = require("os").EOL;
+const path = require("path");
+const fs = require("fs");
 // we want to generate a TOTP in the _future_, and the `notp`
 // library doesn't let us do that unless we set `NODE_ENV = "test"`
 process.env.NODE_ENV = "test";
@@ -8,8 +11,11 @@ const bin = b32.decode(process.env.NPM_OTP_KEY);
 // generate a code 5 seconds in the future
 // 5 seconds is arbitrary, and may need to be tweaked if
 // the release process is discovered to be too fast or too slow.
-const timeOffset = 5000;
+const timeOffset = 1000;
 const now = Date.now();
 const time = now + timeOffset;
 // _t is the time at which we want the OTP to be valid for
-console.log(notp.totp.gen(bin, {_t: time}));
+const otp = notp.totp.gen(bin, {_t: time});
+
+// save to .npmrc so `npm publish` can pick it up later
+fs.appendFileSync(path.join(__dirname, "..", ".npmrc"), `${EOL}otp=${otp}`);
