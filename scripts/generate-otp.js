1f50da418844946b615d66f99587a92462db0f26
Nick Paterno <43416138+nicholasjpaterno@users.noreply.github.com>
2020-05-20 16:03:42 -0400

    Update generate-otp.js

--- scripts/generate-otp.js
+++ scripts/generate-otp.js
@@ -8,8 +8,8 @@ process.env.NODE_ENV = "test";
 const b32 = require("thirty-two");
 const notp = require("notp");
 const bin = b32.decode(process.env.NPM_OTP_KEY);
-// generate a code 5 seconds in the future
-// 5 seconds is arbitrary, and may need to be tweaked if
+// generate a code 1 seconds in the future
+// 1 seconds is arbitrary, and may need to be tweaked if
 // the release process is discovered to be too fast or too slow.
 const timeOffset = 1000;
 const now = Date.now();
